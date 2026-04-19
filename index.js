82a818ca41699615b2c26bcdbc0b1952f1f7886f
User917R <rangerjrgaming@gmail.com>
2026-04-19 23:02:48 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const app = express();
+app.get("/", (req, res) => res.send("Bot running"));
+app.listen(process.env.PORT || 3000);
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "ping") {
+    message.reply("pong");
+  }
+});
+
+client.login(process.env.TOKEN);
